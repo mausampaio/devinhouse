02c0f5ca0382e5f8c7151d65d0d0a74198f0cd6f
Mauricio Sampaio <mausampaio@gmail.com>
2021-01-23 19:39:01 -0300

    [Projeto #2] Criado component InputInteressados

---
+++ modulo-01/semana-06-07/busca-processos/src/components/inputInteressados.jsx
@@ -0,0 +1,45 @@
+import { FiTrash2 } from 'react-icons/fi';
+
+import Input from './input';
+import Button from './button';
+
+const InputInteressados = props => {
+  const { interessado, interessados, setInteressado, setInteressados } = props;
+
+  const handleAdd = () => {
+    if (interessado.trim() === "") {
+      setInteressado("");
+    } else {
+      setInteressados([...interessados, interessado]);
+      setInteressado("");
+    }
+  }
+
+  const handleRemove = index => {
+    const newInteressados = interessados.filter((_, fIndex) => fIndex !== index);
+ 
+    setInteressados(newInteressados);
+  };
+
+  return (
+    <>
+      <div className="interessados">
+        <h4>Interessados</h4>
+        <ul>
+          {interessados.map((item, index) => (
+            <div key={index} className="list-item">
+              <li>{item}</li>
+              <button onClick={() => handleRemove(index)}><FiTrash2 /></button>
+            </div>
+          ))}
+        </ul>
+      </div>
+      <div className="add-interessados">
+        <Input value={interessado} onChange={setInteressado} label="Novo interessado" name="interessados" />
+        <Button color="btn-grey" name="ADICIONAR" onClick={handleAdd} />
+      </div>
+    </>
+  );
+}
+
+export default InputInteressados;
--- modulo-01/semana-06-07/busca-processos/src/components/newModal.jsx
+++ modulo-01/semana-06-07/busca-processos/src/components/newModal.jsx
@@ -1,11 +1,11 @@
-import Input from './input';
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
-import { FiTrash2 } from 'react-icons/fi';
 
 import api from '../services/api';
 
+import Input from './input';
 import Button from './button';
+import InputInteressados from './inputInteressados';
 
 import '../assets/styles/newModal.css';
 
@@ -16,15 +16,6 @@ const NewModal = props => {
   const [interessados, setInteressados] = useState([]);
   const [descricao, setDescricao] = useState("");
   const [assunto, setAssunto] = useState("");
-
-  const handleAdd = () => {
-    if (interessado.trim() === "") {
-      setInteressado("");
-    } else {
-      setInteressados([...interessados, interessado]);
-      setInteressado("");
-    }
-  }
 
   const createProcess = async () => {
     if (interessados.length === 0 || descricao.trim() === "" || assunto.trim() === "") {
@@ -43,12 +34,6 @@ const NewModal = props => {
     toast.success('Processo adicionado com sucesso!')
   }
 
-  const handleRemove = index => {
-    const newInteressados = interessados.filter((_, fIndex) => fIndex !== index);
- 
-    setInteressados(newInteressados);
-  };
-
   useEffect(() => {
     if (process) {
       setAssunto(process.assunto);
@@ -61,25 +46,29 @@ const NewModal = props => {
   return (
     <div className="new-modal-content">
       <div className="new-modal-form">
-        <Input value={assunto} onChange={setAssunto} label="Assunto" name="assunto" />
-        <div className="interessados">
-          <h4>Interessados</h4>
-          <ul>
-            {interessados.map((item, index) => (
-              <div key={index} className="list-item">
-                <li>{item}</li>
-                <button onClick={() => handleRemove(index)}><FiTrash2 /></button>
-              </div>
-            ))}
-          </ul>
-        </div>
-        <div className="add-interessados">
-          <Input value={interessado} onChange={setInteressado} label="Novo interessado" name="interessados" />
-          <Button color="btn-grey" name="ADICIONAR" onClick={handleAdd} />
-        </div>
-        <Input value={descricao} onChange={setDescricao} type="textarea" label="Descrição" />
+        <Input 
+          value={assunto}
+          onChange={setAssunto}
+          label="Assunto"
+          name="assunto"
+        />
+        <InputInteressados
+          interessado={interessado}
+          interessados={interessados}
+          setInteressado={setInteressado}
+          setInteressados={setInteressados}
+        />
+        <Input 
+          value={descricao}
+          onChange={setDescricao}
+          type="textarea" label="Descrição"
+        />
       </div>
-      <Button color="btn-blue" name="SALVAR" onClick={isEdit ? () => toast.error('Um erro inesperado ocorreu!') : createProcess} />
+      <Button
+        color="btn-blue"
+        name="SALVAR"
+        onClick={isEdit ? () => toast.error('Um erro inesperado ocorreu!') : createProcess}
+      />
     </div>
   );
 }
