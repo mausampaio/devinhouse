d71abcafecb51491b8f838ba798e91f88096327b
Mauricio Sampaio <mausampaio@gmail.com>
2021-02-07 18:31:56 -0300

    [SP09] Exercicio 07 concluido

---
+++ modulo-01/semana-09/exercicio-07.sql
@@ -0,0 +1,16 @@
+SELECT * FROM professores 
+WHERE nacionalidade 
+IN (
+  'Argentina',
+  'Bolívia',
+  'Brasil',
+  'Chile',
+  'Colômbia',
+  'Equador',
+  'Guiana',
+  'Paraguai',
+  'Peru',
+  'Suriname',
+  'Uruguai',
+  'Venezuela'
+);
