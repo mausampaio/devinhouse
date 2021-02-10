56edade5a5d1df4b0f47f381b4d1291cea431198
Mauricio Sampaio <mausampaio@gmail.com>
2021-02-10 11:38:47 -0300

    [SP10] Exercicio 03 concluido

---
+++ modulo-01/semana-10/src/exercicios/Exercicio03.java
@@ -0,0 +1,15 @@
+package exercicios;
+
+import java.util.Random;
+
+public class Exercicio03 {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		
+		int n = rand.nextInt(2);
+		
+		System.out.println(n);
+	}
+
+}
