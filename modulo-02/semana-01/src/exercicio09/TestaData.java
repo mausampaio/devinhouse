7eea6cc84b2070714854a205c87dd7c1fa38b742
Mauricio Sampaio <mausampaio@gmail.com>
2021-03-21 12:15:21 -0300

    [SP13] Exercicio 09 concluido

---
+++ modulo-02/semana-01/src/exercicio09/TestaData.java
@@ -0,0 +1,11 @@
+package exercicio09;
+
+public class TestaData {
+
+	public static void main(String[] args) {
+		Data d = new Data(21, 03, 2021);
+		
+		System.out.println(d.exibirData());
+	}
+
+}
