30e7dd7e2909e000a7d65c171fbc0240698cd512
Mauricio Sampaio <mausampaio@gmail.com>
2021-03-31 15:15:58 -0300

    [SP13] Exercicio 15 concluido

--- modulo-02/semana-01/src/exercicio07/ContaBancaria.java
+++ modulo-02/semana-01/src/exercicio07/ContaBancaria.java
@@ -1,11 +1,26 @@
 package exercicio07;
 
 public class ContaBancaria {
+	private static int totalDeContas;
+  private int identificador;
+
 	String nomeDoTitular = "Mauricio";
 	int numero = 1;
 	int agencia = 1;
 	double saldo = 200;
 	double limite = 400;
+
+	public ContaBancaria() {
+		ContaBancaria.totalDeContas++;
+	}
+
+	public ContaBancaria(String nomeDoTitular, int numero, int agencia, String abertaEm) {
+		this();
+		this.identificador = totalDeContas;
+		this.nomeDoTitular = nomeDoTitular;
+		this.numero = numero;
+		this.agencia = agencia;
+}
 	
 	public boolean depositar(double valor) {
 		if (valor <= 0) {
@@ -30,5 +45,9 @@ public class ContaBancaria {
 	public String recuperaDadosParaImpressao() {
 		return "Titular: " + this.nomeDoTitular 
 				+ "%nCC: " + this.numero + "%nAG: " + this.agencia + "%nSaldo: " + this.saldo + "%nLimite: " + this.limite;
+	}
+
+	public int getIdentificador() {
+		return identificador;
 	}
 }
