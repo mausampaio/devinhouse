46312a9dd44656d8e7d35fbf89411bd2bee5d2a7
Mauricio Sampaio <mausampaio@gmail.com>
2021-04-13 15:57:10 -0300

    [SP14] Exercicio 09 concluido

--- modulo-02/semana-02/src/exercicio08/Funcionario.java
+++ modulo-02/semana-02/src/exercicio08/Funcionario.java
@@ -7,9 +7,43 @@ public class Funcionario {
   protected double salario;
 
   public Funcionario(String nome, String sobrenome, String cpf, double salario) {
-      this.nome = nome;
-      this.sobrenome = sobrenome;
-      this.cpf = cpf;
-      this.salario = salario > 0 ? salario : 0.00;
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.cpf = cpf;
+    this.salario = salario > 0 ? salario : 0.00;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getSobrenome() {
+    return sobrenome;
+  }
+
+  public void setSobrenome(String sobrenome) {
+    this.sobrenome = sobrenome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+  
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public double getSalario() {
+    return salario;
+  }
+
+  public void setSalario(double salario) {
+    if(salario > 0) {
+      this.salario = salario;
+    }
   }
 }
--- modulo-02/semana-02/src/exercicio08/Gerente.java
+++ modulo-02/semana-02/src/exercicio08/Gerente.java
@@ -5,8 +5,26 @@ public class Gerente extends Funcionario {
   private int numSubordinados;
 
   public Gerente(String nome, String sobrenome, String cpf, double salario, String senha) {
-      super(nome, sobrenome, cpf, salario);
-      this.senha = senha;
-      this.numSubordinados = 0;
+    super(nome, sobrenome, cpf, salario);
+    this.senha = senha;
+    this.numSubordinados = 0;
+  }
+
+  public String getSenha() {
+    return senha;
+  }
+
+  public void setSenha(String senha) {
+    this.senha = senha;
+  }
+
+  public int getNumSubordinados() {
+    return numSubordinados;
+  }
+
+  public void setNumSubordinados(int numSubordinados) {
+    if(numSubordinados >= 0) {
+      this.numSubordinados = numSubordinados;
+    }
   }
 }
